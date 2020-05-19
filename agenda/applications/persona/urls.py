3df0af31fc507f1ba2675971661f2dcaa70af644
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-19 11:22:59 -0500

    [feat] restframework first method

---
+++ agenda/applications/persona/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from . import views
+
+
+app_name = 'persona_app'
+
+urlpatterns = [
+    path('personas/', views.ListaPersonas.as_view(), name='personas'),
+    path('api/persona/list/', views.PersonListApiView.as_view(),),
+]
